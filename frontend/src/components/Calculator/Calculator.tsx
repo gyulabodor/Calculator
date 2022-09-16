3324167526c0513bc7dbceceebc1b2ad730417e3
gyulabodor <gyulabodor95@gmail.com>
2022-09-16 23:04:32 +0200

    Added fetching functionality to handleSaveClick() and handleShowClick() functions, Added info message to the view

--- frontend/src/components/Calculator/Calculator.tsx
+++ frontend/src/components/Calculator/Calculator.tsx
@@ -1,17 +1,21 @@
 import React, { useState } from "react";
 import "./style.css"
+import { fetchSave } from "../../api";
+import { fetchMemory } from "../../api/getShowRequest";
+
 
 export const Calculator = () => {
     const [operator, setOperator] = useState("");
     const [currentInput, setCurrentInput] = useState("");
     const [prevInput, setPrevInput] = useState("");
-
+    const [infoMessage, setInfoMessage] = useState("");
 
     const evaluate = (arr:string[]) =>{
         return eval(arr.join("")); 
     }
     
     const handleDigitClick = (e : React.MouseEvent<HTMLButtonElement>) => {
+        setInfoMessage("");
 
         const btnValue = e.currentTarget.innerText;
         
@@ -42,7 +46,7 @@ export const Calculator = () => {
         }
     }
     const handleOperatorClick = (e : React.MouseEvent<HTMLButtonElement>) => {
-        
+        setInfoMessage("");
         const btnOperator = e.currentTarget.innerText;
         
         if (currentInput === "" && prevInput === "") {
@@ -58,34 +62,63 @@ export const Calculator = () => {
                 setOperator(btnOperator);
              }
             setPrevInput(evaluate([prevInput,operator,currentInput]));
-            setOperator(btnOperator)
+            setOperator(btnOperator);
             setCurrentInput("");
         }
     }
 
     const handleEvaluateClick = () => {
+        setInfoMessage("");
         setPrevInput(evaluate([prevInput,operator,currentInput]));
         setCurrentInput("");
         setOperator("");
     }
 
     const handleClearClick = (e : React.MouseEvent<HTMLButtonElement>) => {
+        setInfoMessage("");
         setCurrentInput("");
         setPrevInput("");
         setOperator("");
     }
 
+    const handleSaveClick = () =>{
+        setInfoMessage("");
+        const body = { prevInput };
+        
+            fetchSave(body)
+            .then((res) => {
+                setInfoMessage(res.success);
+            })
+            .catch ((err) => {
+                setInfoMessage(err.message);
+            })
+
+
+    };
+    const handleShowClick = () => {
+        fetchMemory()
+        .then((res) => {
+            if(res.number){
+                setPrevInput(res.number)
+            }
+        })
+        .catch((err) => {
+            setInfoMessage(err.message);
+        })
+    }
+
     return (
     
         <div className="calc-wrapper">
+            <div className="info-box">{infoMessage}</div>
             <div className="calc-grid">
                     <div className="screen">
                         <div className="previous-input">{prevInput} {operator}</div>
                         <div className="current-input">{currentInput} </div>
                     </div>
                     <button onClick={handleClearClick}>Clear</button>
-                    <button>Save</button>
-                    <button>Memory</button>
+                    <button onClick={handleSaveClick}>Save</button>
+                    <button onClick={handleShowClick}>Memory</button>
                     <button onClick={handleOperatorClick}>/</button>
                     <button onClick={handleDigitClick}>7</button>
                     <button onClick={handleDigitClick}>8</button>
